a7f9a4946672e03b90addc053c25a4209c6098b1
Kayran <ibrahimmkyrn@gmail.com>
2023-09-15 01:24:55 +0300

    Header ve footer duzenlendi

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,6 +1,10 @@
 import React from "react";
+import Link from "next/link";
+
+import styles from "./styles.module.css";
 
 function Footer() {
-  return <div>Footer</div>;
+  return <footer className={styles.footer}>@2023 Ibrahim Kayran</footer>;
 }
+
 export default Footer;
